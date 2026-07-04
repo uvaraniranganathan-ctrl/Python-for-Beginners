6da74677592fe42455aed8b74bc5e367acea008c
uvaraniranganathan-ctrl <uvaraniranganathan@gmail.com>
2026-07-04 12:31:21 +0530

    Create 01_HelloWorld.py

---
+++ 01_HelloWorld.py
@@ -0,0 +1,12 @@
+# Python Program: Hello World
+
+# Objective:
+# To display a message on the screen.
+
+# print() is a built-in function.
+# "Hello, World!" is a string (text enclosed in quotes).
+
+print("Hello, World!")
+
+Output:
+Hello, World!
