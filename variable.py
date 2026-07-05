ce8a7b3a717498476f97339d54db4c21c13cebc1
uvaraniranganathan-ctrl <uvaraniranganathan@gmail.com>
2026-07-05 22:28:29 +0530

    Update variable.py

--- variable.py
+++ variable.py
@@ -3,12 +3,17 @@
 # Objective:
 # To store and display values.
 
+# Explanation:
+# A variable is a name used to store data.
+# Here, 'name' stores a text value and 'age' stores a number.
+# The print() function displays the values on the screen.
+
 name = "Anju"
 age = 30
 
 print(name)
 print(age)
 
-Output:
-Anju
-30
+# Output:
+# Anju
+# 30
